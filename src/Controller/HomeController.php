be34c1733cbfda387a4b90f0aea0db0907cc7be1
NayanChauveau <nayan.chauveau@gmail.com>
2021-10-19 16:30:48 +0200

    feat: add contact form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\AdvertisingRepository;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
 use App\Repository\PlatformRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdvertisingRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -33,5 +37,29 @@ class HomeController extends AbstractController
         return $this->render('home/cgu.html.twig', []);
     }
 
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(MailerInterface $mailer, Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject($form->get('name')->getData())
+            ->text($form->get('subject')->getData())
+            ;
+
+            $mailer->send($email);
+        }
+
+
+        return $this->render('home/contact.html.twig', [
+            'form' => $form->createView(),
+            'current_menu' => 'contact'
+        ]);
+    }
 }
